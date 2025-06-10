6b518fe9eb674dc554c17470db15823e10b15670
unknown <dbaudtjd258@gmail.com>
2025-06-10 13:53:40 +0900

    refactor: modularize RAG logic in llm.py and clean up chat UI with spinner

--- chat.py
+++ chat.py
@@ -8,33 +8,26 @@ st.title('🤖전세사기피해 상담 챗봇')
 if 'message_list' not in st.session_state:
     st.session_state.message_list = []
 
-# for msg in st.session_state.message_list:
-#     with st.chat_message(msg['role']):
-#         st.write(msg['content'])
-
-print(f'before: {st.session_state.message_list}')
-
 ## 이전 채팅 내용 화면에 출력(표시)
 for message in st.session_state.message_list:
     with st.chat_message(message['role']):
         st.write(message['content'])
 
-
-## prompt 창(채팅 창) #################################################################
+## 채팅 메시지 
 placeholder='전세사기 피해와 관련된 질문을 해주세요.'
-if user_question := st.chat_input(placeholder=placeholder):
+
+if user_question := st.chat_input(placeholder=placeholder): ## prompt 창
+    ## 사용자 메시지
     with st.chat_message('user'):
         ## 사용자 메시지 화면 출력
         st.write(user_question)
     st.session_state.message_list.append({'role':'user','content':user_question})
 
-    ai_message = get_ai_meassge('user_question')
+    ## AI 메시지 
+    with st.spinner('답변을 생성하는 중입니다.'):
+        ai_message = get_ai_meassge(user_question)
 
-    with st.chat_message('ai'):
-        ## AI 메시지 화면 출력
-        st.write(ai_message)
-    st.session_state.message_list.append({'role':'ai', 'content': ai_message})
-
-print(f'after: {st.session_state.message_list}')
-
-print(user_question)
+        with st.chat_message('ai'):
+            ## AI 메시지 화면 출력
+            st.write(ai_message)
+        st.session_state.message_list.append({'role':'ai', 'content': ai_message})
--- llm.py
+++ llm.py
@@ -8,30 +8,46 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 
+## 환경변수 읽어오기 ===================================================================
+load_dotenv()
 
-## [AI message 함수 정의] ##############################
-def get_ai_meassge(user_meassge):
-   
-    ## 환경변수 읽어오기 ###########################################################
-    load_dotenv()
+
+## LLM 함수 정의
+def get_llm(model='gpt-4o'):
+    llm = ChatOpenAI(model=model)
+    return llm
+
+
+## Database 함수 정의 =================================================================
+def get_database():
     PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
-    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
-
-    ## 임베딩 -> 벡터 스토어(데이터베이스)에서 인덱스 가져오기 #####################
+    
     ## 임베딩 모델 지정
     embedding = OpenAIEmbeddings(model='text-embedding-3-large')
     pc = Pinecone(api_key=PINECONE_API_KEY)
     index_name = 'law'
 
-    ## 저장된 인덱스 가져오기 ######################################################
+    ## 저장된 인덱스 가져오기 ========================================================
     database = PineconeVectorStore.from_existing_index(
         index_name=index_name,
         embedding=embedding,
     )
+    
+    return database
 
-    ## RetrievalQA #################################################################
-    llm = ChatOpenAI(model='gpt-4o')
-    prompt = hub.pull('rlm/rag-prompt')
+
+## retrievalQA 함수 정의 ==============================================================
+def get_retrievalQA():
+    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
+
+    ## vector store에서 index 정보
+    database = get_database()
+
+    ## prompt
+    prompt = hub.pull('rlm/rag-prompt', api_key=LANGCHAIN_API_KEY)
+
+    ## LLM 모델 지정
+    llm = get_llm()
 
     def format_docs(docs):
         return '\n\n'.join(doc.page_content for doc in docs)
@@ -45,8 +61,13 @@ def get_ai_meassge(user_meassge):
         | llm
         | StrOutputParser()
     )
+    return qa_chain
 
-    # qa_chain.invoke('전세사기피해자 대상을 알려주세요.')
+
+## [AI message 함수 정의] ==================================
+def get_ai_meassge(user_meassge):
+    qa_chain = get_retrievalQA()
     ai_message = qa_chain.invoke(user_meassge)
+
     return ai_message
 
