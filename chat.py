02fa90f3a1ff4c3660a8473831ed0b1d2cfe6b55
unknown <dbaudtjd258@gmail.com>
2025-06-10 11:05:37 +0900

    feat: implement RAG pipeline in get_ai_message() using Pinecone and GPT-4o

--- chat.py
+++ chat.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import os
+
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone
 
 st.set_page_config(page_title='전세사기피해 상담 챗봇', page_icon='🤖')
 st.title('🤖전세사기피해 상담 챗봇')
@@ -14,12 +23,51 @@ print(f'before: {st.session_state.message_list}')
 
 ## 이전 채팅 내용 화면에 출력(표시)
 for message in st.session_state.message_list:
-    # print('message >>', message)
-    # print('message ["role"] >>', message["role"])
-    # print('message ["content"] >>', message["content"])
-
     with st.chat_message(message['role']):
         st.write(message['content'])
+
+## [AI message 함수 정의] ##############################
+def get_ai_meassge(user_meassge):
+   
+    ## 환경변수 읽어오기 ###########################################################
+    load_dotenv()
+    PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
+
+    ## 임베딩 -> 벡터 스토어(데이터베이스)에서 인덱스 가져오기 #####################
+    ## 임베딩 모델 지정
+    embedding = OpenAIEmbeddings(model='text-embedding-3-large')
+    pc = Pinecone(api_key=PINECONE_API_KEY)
+    index_name = 'law'
+
+    ## 저장된 인덱스 가져오기 ######################################################
+    database = PineconeVectorStore.from_existing_index(
+        index_name=index_name,
+        embedding=embedding,
+    )
+
+    ## RetrievalQA #################################################################
+    llm = ChatOpenAI(model='gpt-4o')
+    prompt = hub.pull('rlm/rag-prompt')
+
+    def format_docs(docs):
+        return '\n\n'.join(doc.page_content for doc in docs)
+
+    qa_chain = (
+        {
+            'context': database.as_retriever() | format_docs,
+            'question': RunnablePassthrough(),
+        }
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    # qa_chain.invoke('전세사기피해자 대상을 알려주세요.')
+    ai_message = qa_chain.invoke(user_meassge)
+    return ai_message
+
+
 ## prompt 창(채팅 창) #################################################################
 placeholder='전세사기 피해와 관련된 질문을 해주세요.'
 if user_question := st.chat_input(placeholder=placeholder):
@@ -28,9 +76,12 @@ if user_question := st.chat_input(placeholder=placeholder):
         st.write(user_question)
     st.session_state.message_list.append({'role':'user','content':user_question})
 
-with st.chat_message('ai'):
-    st.write('여기는 AI 메시지')
-st.session_state.message_list.append({'role':'ai', 'content': '여기는 AI 메시지'})
+    ai_message = get_ai_meassge('user_question')
+
+    with st.chat_message('ai'):
+        ## AI 메시지 화면 출력
+        st.write(ai_message)
+    st.session_state.message_list.append({'role':'ai', 'content': ai_message})
 
 print(f'after: {st.session_state.message_list}')
 
